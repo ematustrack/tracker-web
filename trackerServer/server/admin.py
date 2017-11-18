4dfe6b791e81b9546285ce6791569a9514d257b1
Ubuntu <ubuntu@ip-172-26-13-39.ec2.internal>
2017-11-18 22:20:05 +0000

    maniputaling st_folio form admin

--- trackerServer/server/admin.py
+++ trackerServer/server/admin.py
@@ -5,6 +5,22 @@ from django.contrib import admin
 
 from .models import *
 
+class StFolioAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):
+		st_input = obj.idST
+		result = None
+		try:
+			result=St_work.objects.filter(idSTFolio__idST=st_input).first()
+			if result != None:
+				work=Work(name=result.idObra.name)
+				obj.save()
+				st=St_work(idObra=work, idSTFolio=obj)
+				st.save()
+			else:
+				obj.save()
+		except:
+			obj.save()
+
 admin.site.register(St_work)
 admin.site.register(St_folio)
 
